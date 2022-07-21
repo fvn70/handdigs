dd30421a2d67643cc143ec98df16d3d5c276ae23
fvn70 <fvn_reg@mail.ru>
2022-07-21 12:01:39 +0700

    Stage 2/5: Split into sets

--- Classification of Handwritten Digits/task/analysis.py
+++ Classification of Handwritten Digits/task/analysis.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
@@ -8,7 +10,12 @@ rows = x_train.shape[0]
 x_train = x_train.reshape(rows, cols)
 y_class = np.unique(y_train)
 
-print('Classes: ', y_class)
-print("Features' shape:", x_train.shape)
-print("Target' shape:", y_train.shape)
-print(f"min: {x_train.min()}, max: {x_train.max()}")
+x_train, x_test, y_train, y_test = train_test_split(x_train[:6000], y_train[:6000], train_size=0.7, random_state=40)
+
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
+print('y_train shape:', y_train.shape)
+print('y_test shape:', y_test.shape)
+
+print('Proportion of samples per class in train set:')
+print(pd.Series(y_train).value_counts(normalize=True))
