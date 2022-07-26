3d8b2edadcc9a0787bc1b6c3e3e89537e5f2ce5d
fvn70 <fvn_reg@mail.ru>
2022-07-26 12:07:37 +0700

    Stage 5/5: Hyperparameter tuning

--- Classification of Handwritten Digits/task/analysis.py
+++ Classification of Handwritten Digits/task/analysis.py
@@ -8,7 +8,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import Normalizer
-
+from sklearn.model_selection import GridSearchCV
 
 def fit_predict_eval(model, features_train, features_test, target_train, target_test, prt=False):
     # here you fit the model
@@ -21,6 +21,24 @@ def fit_predict_eval(model, features_train, features_test, target_train, target_
     if prt:
         print(f'Model: {model}\nAccuracy: {round(score, 3)}\n')
     return score
+
+
+def stage_4():
+    best = {}
+    comp = [0, 0]
+    for i in range(4):
+        acc = fit_predict_eval(models[i], x_train, x_test, y_train, y_test)
+        acc1 = fit_predict_eval(models[i], x_train_norm, x_test_norm, y_train, y_test, True)
+        best[model_names[i]] = acc1
+        if acc1 > acc:
+            comp[1] += 1
+        else:
+            comp[0] += 1
+    # print(best)
+    sort = sorted(best, key=best.get, reverse=True)
+    # print(sort)
+    print(f"The answer to the 1st question: {'yes' if comp[1] > comp[0] else 'no'}")
+    print(f'The answer to the 2nd question: {sort[0]} - {round(best[sort[0]], 3)}, {sort[1]} - {round(best[sort[1]], 3)}')
 
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
@@ -44,18 +62,31 @@ model_names = ['KNeighborsClassifier',
                'LogisticRegression',
                'RandomForestClassifier']
 
-best = {}
-comp = [0, 0]
-for i in range(4):
-    acc = fit_predict_eval(models[i], x_train, x_test, y_train, y_test)
-    acc1 = fit_predict_eval(models[i], x_train_norm, x_test_norm, y_train, y_test, True)
-    best[model_names[i]] = acc1
-    if acc1 > acc:
-        comp[1] += 1
-    else:
-        comp[0] += 1
-# print(best)
-sort = sorted(best, key=best.get, reverse=True)
-# print(sort)
-print(f"The answer to the 1st question: {'yes' if comp[1] > comp[0] else 'no'}")
-print(f'The answer to the 2nd question: {sort[0]} - {round(best[sort[0]], 3)}, {sort[1]} - {round(best[sort[1]], 3)}')
+model = KNeighborsClassifier()
+parametrs = {'n_neighbors': [3, 4],
+              'weights': ['uniform', 'distance'],
+              'algorithm': ['auto', 'brute']}
+grid = GridSearchCV(model, parametrs, scoring='accuracy', n_jobs=-1)
+grid.fit(x_train_norm, y_train)
+nn = grid.best_params_['n_neighbors']
+we = grid.best_params_['weights']
+al = grid.best_params_['algorithm']
+
+print('K-nearest neighbours algorithm')
+print(f"best estimator: KNeighborsClassifier(n_neighbors={nn}, weights={we}, algorithm={al})")
+acc = fit_predict_eval(KNeighborsClassifier(n_neighbors=nn, weights=we, algorithm=al), x_train_norm, x_test_norm, y_train, y_test)
+print(f"accuracy: {round(acc, 3)}")
+
+model = RandomForestClassifier(random_state=40)
+parametrs = {'n_estimators': [300, 500],
+             'max_features': ['sqrt', 'log2'],
+             'class_weight': ['balanced', 'balanced_subsample']}
+grid = GridSearchCV(model, parametrs, scoring='accuracy', n_jobs=-1)
+grid.fit(x_train_norm, y_train)
+cw = grid.best_params_['class_weight']
+mf = grid.best_params_['max_features']
+ne = grid.best_params_['n_estimators']
+print('Random forest algorithm')
+print(f"best estimator: RandomForestClassifier(class_weight={cw}, max_features={mf}, n_estimators={ne})")
+acc = fit_predict_eval(RandomForestClassifier(random_state=40, class_weight=cw, max_features=mf, n_estimators=ne), x_train_norm, x_test_norm, y_train, y_test)
+print(f"accuracy: {round(acc, 3)}")
